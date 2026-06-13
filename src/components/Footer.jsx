d5268ec2feb44e52bd58eff12b07c1ad0449fed8
Prathmesh Naphade <prathameshnaphade58@gmail.com>
2026-06-14 03:33:10 +0530

    linkdein setup

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <footer className="py-8 text-center border-t border-white/10 text-gray-400 bg-black/20">
-      <p ><a href="www.linkedin.com/in/prathmesh-naphade"> By Prathmesh Naphade</a></p>
+      <p ><a href="https://www.linkedin.com/in/prathmesh-naphade"
+        target="_blank"
+        rel="noopener noreferrer"> By Prathmesh Naphade</a></p>
     </footer>
   );
 };
